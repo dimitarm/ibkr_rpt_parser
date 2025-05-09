381e5a37f22a3a08275decd0b8c70ed52bfafd89
Dimitar Margaritov <dimitar_margaritov@yahoo.com>
2025-05-09 10:14:27 +0300

    add support for input parameters

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <OpenXLSX.hpp>
+#include <boost/program_options.hpp>
 #include <boost/tokenizer.hpp>
 #include <boost/algorithm/string/replace.hpp>
+#include <boost/algorithm/string/erase.hpp>
 #include <sstream>
 #include <fstream>
 #include <string>
@@ -12,6 +14,9 @@ string normalize(const string &line)
     string result;
     bool first_esc = false;
     string::size_type last_esc = string::npos;
+    if (line.find("Date/Time") != string::npos){
+        cout << line << endl;
+    }
     for (auto it = line.begin(); it != line.end(); it++)
     {
         if (static_cast<unsigned char>(*it) <= 0x7f && *it != ';')
@@ -28,6 +33,9 @@ string normalize(const string &line)
             }
             else
             {
+                if(first_esc && *it == ','){
+                    continue;
+                }
                 result.push_back(*it);
             }
         }
@@ -37,30 +45,66 @@ string normalize(const string &line)
         result.erase(last_esc, 1);
     }
     boost::algorithm::replace_all(result, "\"\"", "\"");
+    //boost::algorithm::erase_all(result, ",");
 
 //    cout << "line:" << result << endl;
     return result;
 }
 
-int main()
+int main(int argc, char* argv[])
 {
-    ifstream infile("report.csv");
+    namespace po = boost::program_options;
+    po::options_description desc("Allowed options");
+    desc.add_options()
+        ("help", "<help message>")
+        ("input-file,i", po::value<string>(), "input file")
+        ("output-file,o", po::value<string>(), "output file")
+    ;
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    if (vm.count("input-file") != 1){
+        cerr << "input-file is not specified." << endl;
+    }
+
+    ifstream infile(vm["input-file"].as<string>());
     if (infile.fail())
     {
-        cout << "File cannot ne opened." << endl;
+        cerr << "Input file cannot be opened." << endl;
         return -1;
     }
     typedef boost::tokenizer<boost::escaped_list_separator<char> , std::string::const_iterator, std::string> Tokenizer;
     
     OpenXLSX::XLDocument doc;
-    doc.create("report.xlsx");
+    switch(vm.count("output-file")){
+        case 0:
+            {
+                string output_file = vm["input-file"].as<string>();
+                auto last_separator_pos = output_file.find_last_of(".");
+                if(last_separator_pos != string::npos){
+                    output_file.erase(last_separator_pos);
+                }
+                output_file.append(".xlsx");
+                cout << "Output file: " << output_file << endl;
+                doc.create(output_file, true);
+            }
+            break;
+        case 1:
+            doc.create(vm["output-file"].as<string>(), true);
+            break;
+        default:
+            cerr << "More than one output-file parameter" << endl;
+            return -1;
+    }
+
     auto workbook = doc.workbook();
 
     string line;
     while (getline(infile, line))
     {
-        string normalized_line = normalize(line);
-        Tokenizer tok(normalized_line, boost::escaped_list_separator<char>('\\', ',', '\"'));
+        // string normalized_line = normalize(line);
+        Tokenizer tok(line, boost::escaped_list_separator<char>("", ",", "\""));
         if (tok.begin() != tok.end())
         {
             auto worksheet_name = *(tok.begin());
@@ -101,7 +145,7 @@ int main()
         }
         else
         {
-            cout << "Skipping empty line:  " << normalized_line << endl;
+            cout << "Skipping empty line:  " << line << endl;
         }
     }
     doc.save();
